581a1ca31d761b26b1d47ac624234a4ea618572d
millicentagbo <millicentagbo44@gmail.com>
2025-05-26 22:36:03 +0100

    multi lines added

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -6,4 +6,13 @@ const lines = [
   "JavaScript is amazing"
 ];
 
-console.log(lines.join('\n'));
+let output = '';
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i];
+  if (i < lines.length - 1) {
+    output += '\n';
+  }
+}
+
+console.log(output);
+
